935956b6cf9b0eb4ea41ddaa56f4253cd30c09c3
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-07-21 12:52:48 -0300

    Implemented functionality

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from pytube import YouTube
 
-url = input("Enter the YouTube video URL: ")
+def VideoInfo(video):
+    print("Title: " + video.title)
+    print("File name: " + video.default_filename)
+    print("Size (mb): " + str(video.filesize_mb))
 
-yt = YouTube(url)
+def DownloadVideo(link):
+    yt = YouTube(link)
+    yt = yt.streams.get_by_resolution("720p")
+
+    try: 
+        yt.download()
+    except: 
+        print("There was an error downloading the video " + yt.title)
+
+    print("Download done successfully")
+    VideoInfo(yt)
+
+url = input("Enter the YouTube video URL: ")
+DownloadVideo(url)
